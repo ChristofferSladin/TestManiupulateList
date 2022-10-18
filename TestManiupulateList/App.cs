a62967cb8c34274d0d1994822dd7c21336a7dab1
Christoffer S <christoffer.sladin@hotmail.com>
2022-10-18 13:31:28 +0200

    Add project files.

---
+++ TestManiupulateList/App.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestManiupulateList
+{
+    public class App
+    {
+        public void Run()
+        {
+            ChangeObj();
+        }
+
+        public void ChangeObj()
+        {
+            var products = new List<Product>();
+
+            products.Add(new Product(11, "banan", "st", 30m));
+            products.Add(new Product(12, "apa", "st", 20m));
+            products.Add(new Product(13, "kung", "kg", 15m));
+            products.Add(new Product(14, "bajs", "kg", 75m));
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Id} {product.Name} {product.Price} {product.PriceType}");
+            }
+
+            Console.WriteLine("hitta id");
+            var myValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nytt namn");
+            var newName = Console.ReadLine();
+            Console.WriteLine("Nytt pris");
+            var newPrice = decimal.Parse(Console.ReadLine());
+
+            var obj = products.FirstOrDefault(x => x.Id == myValue);
+
+            if (obj != null)
+            {
+                obj.Name = newName;
+                obj.Price = newPrice;
+            }
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Id} {product.Name} {product.Price} {product.PriceType}");
+            }
+        }
+
+    }
+}
---
+++ TestManiupulateList/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestManiupulateList
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PriceType { get; set; }
+        public decimal Price { get; set; }
+
+
+
+        public Product(int id, string name, string pricType, decimal price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.PriceType = pricType;
+            this.Price = price;
+        }
+    }
+
+
+   
+}
